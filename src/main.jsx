6d800819739702bcd8d0497f9cdef4c8cfe47671
Rolando Medina <rolando.ms@outlook.com>
2023-04-27 22:16:26 -0500

    fix: move fonts import from main to App component

--- src/main.jsx
+++ src/main.jsx
@@ -1,37 +1,13 @@
 // React
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-// CC
-import theme from './utils/theme';
-import App from './App'
-import { ChakraProvider } from '@chakra-ui/react';
 // Style
-import '@fontsource/inter';
-import '@fontsource/inter/100.css'
-import '@fontsource/inter/200.css'
-import '@fontsource/inter/300.css'
-import '@fontsource/inter/400.css'
-import '@fontsource/inter/500.css'
-import '@fontsource/inter/600.css'
-import '@fontsource/inter/700.css'
-import '@fontsource/inter/800.css'
-import '@fontsource/inter/900.css'
-import '@fontsource/montserrat';
-import '@fontsource/montserrat/100.css';
-import '@fontsource/montserrat/200.css';
-import '@fontsource/montserrat/300.css';
-import '@fontsource/montserrat/400.css';
-import '@fontsource/montserrat/500.css';
-import '@fontsource/montserrat/600.css';
-import '@fontsource/montserrat/700.css';
-import '@fontsource/montserrat/800.css';
-import '@fontsource/montserrat/900.css';
 import './index.css';
+// CC
+import App from './App'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <ChakraProvider theme={theme}>
-      <App />
-    </ChakraProvider>
-  </React.StrictMode>,
+    <App />
+  </React.StrictMode>
 )
