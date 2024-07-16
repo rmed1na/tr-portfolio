3d6e143fdd0b40730270a8328f320055c2117ae8
xthan1am <thania1896@gmail.com>
2024-07-16 13:19:43 -0500

    Update Home.jsx
    Update of CTA ui, and home text.
    Removal of contact me link

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -29,30 +29,29 @@ export default function Home() {
                     <Box fontSize={['0.875rem', '1.125rem']}>
                         <HStack spacing={5}>
                             <Link>About Me</Link>
-                            <Link>Contact</Link>
                         </HStack>
                     </Box>
                 </Flex>
                 <Flex mt={5} ml={[6, 12]} mr={[6, 0]} justify='space-between' direction={['column', 'row']}>
                     <Box w={['100%', '50%']} color='#454545'>
-                        <Heading fontSize={['1.625rem', '2.5rem']} fontWeight={700} mb={4}>Brand & UX/UI Designer</Heading>
-                        <Text fontSize={['1rem' ,'1.5rem']} fontFamily='Montserrat' mb={5}>I help clients around the world find their brand identity and create engaging experiences through design.</Text>
+                        <Heading fontSize={['1.625rem', '2.5rem']} fontWeight={700} mb={4}>Hey there! I'm Thania </Heading>
+                        <Text fontSize={['1rem' ,'1.5rem']} fontFamily='Montserrat' mb={5}>Welcome to my portfolio🫶🏻. As a seasoned UX/UI designer, I bring a holistic approach to every project, seamlessly integrating my extensive background in marketing, content creation and conversion rate optimization. This diverse expertise allows me to craft intuitive and visually compelling designs that not only engage users but also drive results. Explore my work and see how I transform ideas into impactful user experiences.</Text>
                         <Button 
                             mt={5}
                             mb={10}
-                            borderRadius='56px'
+                            borderRadius='16px'
                             fontSize={['1rem' ,'1.25rem']}
                             fontWeight={700}
                             py={['1rem', '1.5rem']}
                             px='1.5rem'
-                            bg='linear-gradient(180deg, #DB239C 0%, #3D1878 100%)'
+                            bg='#292C35'
                             color='white'
                             boxShadow='3px 4px 6px -3px rgba(0,0,0,0.75)'
                             _hover={{
                                 bg: 'linear-gradient(90deg, #DB239C 0%, #3D1878 100%)'
                             }}
                             onClick={handleMailClick}
-                        >Let's work</Button>
+                        >Let's Collaborate</Button>
                     </Box>
                     <Box 
                         w={['100%', '50%']} 
